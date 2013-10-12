22baeca001a5524a3cc63d7ad89fac8091b67f63
Austin Chen <akrolsmir@gmail.com>
2013-10-12 14:09:17 -0700

    Realtime parsing of notes

--- noteify/public/js/high.js
+++ noteify/public/js/high.js
@@ -1,16 +1,20 @@
 var dancer = new Dancer();
 var array = 0;
 var note;
-var notes = [];
+var notes = [{scientific: function(){return "R";}, "duration": 1}];
 var index = 0;
 var prev2 = "";
 var scientific = "";
 var pause_count = 0;
 var last_time = 0;
-var MIN_LENGTH = 10;
+var last_note = 0;
+var last_ampl = 0.000001;
+var MIN_LENGTH = 8;
 var VOLUME_THRESHOLD = .01;
-var SAMPLING_RATE = 22050; //change this to your computer's sampling rate
-// var SAMPLING_RATE = 24000; //change this to your computer's sampling rate
+// var SAMPLING_RATE = 22050; //change this to your computer's sampling rate
+var SAMPLING_RATE = 24000; //change this to your computer's sampling rate
+
+var LOW_NOTE = "C4", HIGH_NOTE = "C6";
 
 
 // These are the final results
@@ -19,9 +23,9 @@ var durations = [];
 
 // This starts the playing/parsing
 function getNotes(url, callback) {
-  dancer.load({ src: '/data/?file=' + url });
-  dancer.setVolume(0);
-  // dancer.load({ src: url }); //TODO remove
+  // dancer.load({ src: '/data/?file=' + url });
+  // dancer.setVolume(0);
+  dancer.load({ src: url }); //TODO remove
   dancer.play();
 
   dancer.after(0, function(){
@@ -33,69 +37,138 @@ function getNotes(url, callback) {
   last_time = dancer.getTime();
 
   var merged = false;
-  var freq = fuzzy_index(array) * SAMPLING_RATE / 512;
-  //console.log(freq);
-  if(!isNaN(freq) && freq > 0){
-    pause_count = 0;
-    note = teoria.note.fromFrequency(freq);
-    if(scientific == note.note.scientific()){
-      notes[index]["duration"] += 1; 
-      console.log(scientific);
-    } else {
-      index++;
-      scientific = note.note.scientific();
-      notes[index] = {};
-      notes[index]["note"] = scientific;
-      notes[index]["duration"] = 1;
-      console.log(note.note.scientific());
+  var temp = fuzzy_index(array);
+  var freq = temp[0] * SAMPLING_RATE / 512, ampl = temp[1];
+
+////
+  var note;
+  if(freq == 0 || isNaN(freq)){
+    note = {scientific: function(){return "R";}};
+  } else{
+    note = teoria.note.fromFrequency(freq).note;
+  }
+
+  console.log(note.scientific());
+
+  if(note.scientific() != last_note || // if new note
+    (ampl - last_ampl) / last_ampl > 3){ // or 3 times louder
+
+    // create new note
+    index++;
+    notes[index] = {"note": note, "duration" : 1};
+
+    // maybe save current note
+    // debugger;
+    if(index > 1 && notes[index-1].duration >= MIN_LENGTH && ((QUARTER > 0) || last_note != 0)){
+      if(QUARTER < 0){
+        QUARTER = notes[index].duration;
+      }
+
+      var temp = parseNote(notes[index]);
+      callback(temp[0], temp[1]);
     }
   } else{
-    if(freq == 0){
-      console.log("gap");
-      index++;
-      notes[index] = {};
-      notes[index]["note"] = "tongue";
-      notes[index]["duration"] = 1;
-      scientific = 0;
-    } else {
-      // console.log(" ");
-    }
-
-    if(pause_count > 30){
-      dancer.pause();
-      // console.log("FILTERED, BITCHEEEEES!!!!");
-      filter(notes);
-      convert(notes)
-      for(var i = 0; i < notesarr.length; i++){
-        console.log(notesarr[i][0] + " " + durations[i]);
-      }
-      callback(notesarr, durations);
-    }
+    // increment current note
+    notes[index].duration++;
   }
+  last_note = note.scientific();
+  last_ampl = ampl;
+
+  if(pause_count > 10 || dancer.getTime() > 30){
+    dancer.pause();
+    return;
+  }
+
+
+////
+  //console.log(freq);
+  // if(!isNaN(freq) && freq > 0){
+  //   pause_count = 0;
+  //   note = teoria.note.fromFrequency(freq);
+  //   if(scientific == note.note.scientific()){
+  //     notes[index]["duration"] += 1; 
+  //     console.log(scientific);
+  //   } else {
+  //     index++;
+  //     scientific = note.note.scientific() + "\t~" + result[1];
+  //     notes[index] = {};
+  //     notes[index]["note"] = scientific;
+  //     notes[index]["duration"] = 1;
+  //     console.log(note.note.scientific());
+
+  //     if(notes[index-1].duration >= MIN_LENGTH){
+  //       document.writeln(notes[index-1].note + "--" + notes[index-1].duration + "\n");
+  //     }
+  //   }
+  // } else{
+  //   if(freq == 0){
+  //     console.log("gap");
+  //     index++;
+  //     notes[index] = {};
+  //     notes[index]["note"] = "tongue";
+  //     notes[index]["duration"] = 1;
+  //     scientific = 0;
+
+  //     if(notes[index-1].duration >= MIN_LENGTH){
+  //       document.writeln(notes[index-1].note + "--" + notes[index-1].duration + "\n");
+  //     }
+  //   }
+
+  //   if(pause_count > 30 || dancer.getTime() > 10){
+  //     dancer.pause();
+  //     // console.log("FILTERED, BITCHEEEEES!!!!");
+  //     filter(notes);
+  //     convert(notes)
+  //     for(var i = 0; i < notesarr.length; i++){
+  //       console.log(notesarr[i][0] + " " + durations[i]);
+  //     }
+  //     callback(notesarr, durations);
+  //   }
+  // }
   });
 
-function filter(array){
-  for(var i = 1; i < array.length; i++){
-    //console.log(array[i]["duration"]);
-    if(array[i]["duration"] < MIN_LENGTH && array[i]["note"] != "tongue"){
-      array.splice(i,1);
-      if(i > 1 && array[i]["note"] == array[i-1]["note"]){ //merge
-        array[i]["duration"] += array[i-1]["duration"];
-        array.splice(i-1,1);
-      }
-      i--;
-    }
+function parseNote(note){
+  //figure out length
+  var length;
+  if(note.duration < QUARTER * 0.5){
+    length = '8';
+  } else if(note.duration < QUARTER * 1.5){
+    length = 'q';
+  } else if(note.duration < QUARTER * 3){
+    length = 'h';
+  } else{ //if(note.duration < QUARTER * 5){
+    length = 'w';
   }
-  for(var i = 1; i < array.length; i++){
-    if(array[i]["note"] == "tongue"){
-      array.splice(i,1);
-      i--;
-    }
+
+  if(note.note.scientific() == "R"){
+    return ["c/5", length + "r"];
+  } else{
+    return [process(note.note.scientific()), length];
   }
-    //return array;
-  }
+}
+
+// function filter(array){
+
+//   for(var i = 1; i < array.length; i++){
+//     //console.log(array[i]["duration"]);
+//     if(array[i]["duration"] < MIN_LENGTH && array[i]["note"] != "tongue"){
+//       array.splice(i,1);
+//       if(i > 1 && array[i]["note"] == array[i-1]["note"]){ //merge
+//         array[i]["duration"] += array[i-1]["duration"];
+//         array.splice(i-1,1);
+//       }
+//       i--;
+//     }
+//   }
+//   for(var i = 1; i < array.length; i++){
+//     if(array[i]["note"] == "tongue"){
+//       array.splice(i,1);
+//       i--;
+//     }
+//   }
+//     //return array;
+//   }
   function fuzzy_index(array){
-    var gap = false;
     var max = 0;
     var a = 0;
     for(var i = 0; i < array.length; i++){
@@ -105,38 +178,36 @@ function filter(array){
       }
     }
     if(array[a] < VOLUME_THRESHOLD){
-      gap = true;
+      return[0, 0];
     }
     var b = a + (array[a+1] > array[a-1] ? 1 : -1);
-
-    if(gap){
-      return 0;
-    }
-    return (array[a] * a + array[b] * b) / (array[a] + array[b]);
+    return [(array[a] * a + array[b] * b) / (array[a] + array[b]), array[a] + array[b]];
   }
 
-  var NOTE_LENGTH = 35;
+  var QUARTER = -1;
 
-  function convert(array){
-    // hackily sets quarter note length to the second item.
-    NOTE_LENGTH = notes[2].duration;
-    for(var i = 1; i < array.length; i++){
-      var note = notes[i];
-      console.log(note);
-      notesarr[i-1] = [process(note.note)];
-      if(note.duration < NOTE_LENGTH * 1.5){
-        durations[i-1] = 'q';
-      } else if(note.duration < NOTE_LENGTH * 3){
-        durations[i-1] = 'h';
-      } else if(note.duration < NOTE_LENGTH * 5){
-        durations[i-1] = 'w';
-      }
-    }
-  }
+  // function convert(array){
+  //   // hackily sets quarter note length to the second item.
+  //   // QUARTER = notes[2].duration;
+  //   for(var i = 1; i < array.length; i++){
+  //     var note = notes[i];
+  //     console.log(note);
+  //     notesarr[i-1] = [process(note.note)];
+  //     if(note.duration < QUARTER * 0.5){
+  //       durations[i-1] = '8';
+  //     } else if(note.duration < QUARTER * 1.5){
+  //       durations[i-1] = 'q';
+  //     } else if(note.duration < QUARTER * 3){
+  //       durations[i-1] = 'h';
+  //     } else if(note.duration < QUARTER * 5){
+  //       durations[i-1] = 'w';
+  //     }
+  //   }
+  // }
 
-  // Converts A5 to a/4
+  // Converts A5 to a/5
   function process(note){
     note = note.toLowerCase();
-    return note.slice(0, -1) + '/' + (parseInt(note.slice(-1)) - 1);
+    return note.slice(0, -1) + '/' + (parseInt(note.slice(-1)));
   }
 }

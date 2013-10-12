1c8a43a4408e185c755923de8f987b85a431ba03
Evan Ye <applepie31415@gmail.com>
2013-10-12 11:28:07 -0700

    trim videos to 30 seconds

--- noteify/util.js
+++ noteify/util.js
@@ -1,6 +1,7 @@
 var ytdl = require('ytdl');
 var ffmpeg = require('fluent-ffmpeg');
 
+var trim = 30; // 30 seconds is the longest video
 var counter = 1;
 
 module.exports = {
@@ -8,11 +9,17 @@ module.exports = {
     // Download Youtube video as WAV
     var stream = ytdl(url);
     var wavFile = './data/' + counter + '.wav';
-    var proc = new ffmpeg({source: stream})
-      .toFormat('wav')
-      .saveToFile(wavFile, function(stdout, stderr){
-        callback(counter + '.wav');
-        counter++;
-      });
+
+    try {
+      var proc = new ffmpeg({source: stream})
+        .toFormat('wav')
+        .setDuration(30)
+        .saveToFile(wavFile, function(stdout, stderr){
+          callback(counter + '.wav');
+          counter++;
+        });
+    } catch (error){
+      console.log('meh, error. hopefully ok');
+    }
   }
 }
